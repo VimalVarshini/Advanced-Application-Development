42a3c500a52a86eb6cda1e9ba73bba8cc8d59f6d
VimalVarshini <vimalvarshini1206@gmail.com>
2024-03-20 21:17:23 +0530

    Day 4 - User Dashboard

--- advanced/src/App.jsx
+++ advanced/src/App.jsx
@@ -7,6 +7,13 @@ import Adminnav from './Components/Admin/Adminnav';
 import AdminCompanyhome from './Components/Admin/AdminCompanyhome';
 import Adminsidebar from './Components/Admin/Sidebar/Adminsidebar';
 
+import Companyhome from './Components/Home/Companyhome';
+import Jobhome from './Components/Home/Jobhome';
+import Applycard from './Components/Home/Applycard/Applycard';
+import Paymentcard from './Components/Home/Paymentcard/Paymentcard';
+import Addjobcard from './Components/Admin/Addjobcard/Addjobcard';
+
+const Home = lazy(() => import("./Components/Home/Home"));
 const Login = lazy(() => import("./Components/Login/Login"));
 const Register = lazy(() => import("./Components/Register/Register"));
 
@@ -24,6 +31,13 @@ function App() {
           <Route path="/admincompany" exact element={<AdminCompanyhome/>} />
           <Route path="/adminnav" exact element={<Adminnav/>} />
           <Route path="/adminnsidebar" exact element={<Adminsidebar/>} />
+          <Route path="/register" exact element={<Register/>} />
+          <Route path="/home" exact element={<Home/>} />
+          <Route path="/company" exact element={<Companyhome/>} />
+          <Route path="/jobs" exact element={<Jobhome/>} />
+          <Route path="/applycard" exact element={<Applycard/>} />
+          <Route path="/paymentcard" exact element={<Paymentcard/>} />
+          <Route path="/addjob" exact element={<Addjobcard/>} />
         </Routes>
       </Suspense>
     </BrowserRouter>
